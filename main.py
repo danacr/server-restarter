f54e9566cc4a966c81086439d1089820a33243c6
danacr <dan@acristinii.com>
2023-03-15 23:44:18 +0100

    bytes again?

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
 import time
+import subprocess
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 def restart():
-    os.system("echo -n -e '\x00\xf1' >/dev/hidraw0")
+    dev = os.open("/dev/hidraw0", os.O_RDWR)
+    os.write(dev,b'\x00'b'\x01')
     time.sleep(1)
-    os.system("echo -n -e '\x00\x01' >/dev/hidraw0")
-    
+    os.write(dev,'\x00'b'\xf1')
+
 @app.route("/", methods=['GET', 'POST'])
 def control():
     if request.method == 'POST':
-        restart()
+        match request.form.get('action'):
+            case "Restart":
+                subprocess.run([busylight_path, 'on', 'green'],
+                               stdout=subprocess.PIPE)
     return render_template('index.html')
 
 if __name__ == '__main__':
