d84ef511f651e11e7f508b5e7f57a2171f791fab
danacr <dan@acristinii.com>
2023-03-15 23:34:18 +0100

    maybe echo?

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import os
+import subprocess
 import time
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 def restart():
-    dev = os.open("/dev/hidraw0", os.O_RDWR)
-    os.write(dev,str.encode("\x00\x01"))
+    subprocess.run(['echo', '-n', '-e', '\x00\x01', '>', '/dev/hidraw0'],
+                               stdout=subprocess.PIPE)
     time.sleep(1)
-    os.write(dev, str.encode("\x00\xf1"))
-
+    subprocess.run(['echo', '-n', '-e', '\x00\xf1', '>', '/dev/hidraw0'],
+                               stdout=subprocess.PIPE)
+    
 @app.route("/", methods=['GET', 'POST'])
 def control():
     if request.method == 'POST':
